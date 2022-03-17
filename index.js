36b4ac953f62fed1ee794e02b67cf3b13ea23195
mutuajoseph <wambuajoseph756@gmail.com>
2022-03-17 13:32:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+let customerName = 'bob'
+let leastFavoriteCustomer = 'some initial value'
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase()
+
+  //notice that there is no return statement
+  //if you try to use `return customerName.toUpperCase()`, it will return 'BOB', but will not redefine the variable!
+}
+
+function setBestCustomer() {
+  let bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+  let bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'billy'
+}
